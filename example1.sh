34f25de5ef62644bb19f448c9d52607f673482d0
AlexCue987 <alex.cue.987@gmail.com>
2017-08-27 14:24:07 -0500

    simplified shell scripts

--- example1.sh
+++ example1.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat sampleInput1.txt | java -cp out/artifacts/game_of_life_expansion_jar/game_of_life_expansion.jar org.barren.land.Main
+cat sampleInput1.txt | java -jar out/artifacts/game_of_life_expansion_jar/game_of_life_expansion.jar
--- example2.sh
+++ example2.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cat sampleInput2.txt | java -cp out/artifacts/game_of_life_expansion_jar/game_of_life_expansion.jar org.barren.land.Main
+cat sampleInput2.txt | java -jar out/artifacts/game_of_life_expansion_jar/game_of_life_expansion.jar
